3a4fd6725b6229ccdc46a71f2775cff8e26a49cc
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-03-29 21:27:29 +0600

    fixed all calculate with deep

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -11,20 +11,20 @@ const Shop = () => {
     useEffect(() => {
         // fetch url
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
+            .then(res => res.json())
+            .then(data => setProducts(data))
     }, []);
 
-    useEffect( () => {
+    useEffect(() => {
         const storedCart = getShoppingCart();
         let savedCart = [];
 
         // step 01- get id
-        for(const id in storedCart){
+        for (const id in storedCart) {
             // step 02- (get the product using id) find with products for checking weather id is matched or not.
             const addedProduct = products.find(product => product.id === id)
             // step 03- get quantity of the product
-            if(addedProduct){
+            if (addedProduct) {
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
                 // step 04- add the addedProduct to the savedCart
@@ -34,23 +34,54 @@ const Shop = () => {
             setCart(savedCart);
             console.log(addedProduct);
         }
-    }, [products]) 
+    }, [products])
 
+    
     const handlerAddToCart = (product) => {
         // console.log(product);
         // React JS এ array তে push করা যায় না, নতুন array বানিয়ে পাঠানো লাগে set function এ।
         const newCart = [...cart, product];
         setCart(newCart);
 
-        // এই function টা just id parameter হিসেবে নিবে। 
+
+        // এই function টা just id parameter হিসেবে নিবে।
         addToDb(product.id)
     }
+    
+
+
+    /*
+    const handlerAddToCart = (product) => {
+        // console.log(product);
+        // React JS এ array তে push করা যায় না, নতুন array বানিয়ে পাঠানো লাগে set function এ।
+        let newCart = [];
+
+
+        // if product doesn't exist in the cart, then set quantity = 1
+        // if exist , update quantity by 1
+        const exists = cart.find(pd => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            // newCart = [...Cart, product];
+            newCart = [...Cart, product]
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+        // এই function টা just id parameter হিসেবে নিবে।
+        addToDb(product.id)
+    }
+    */
+
+
 
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product 
+                    products.map(product => <Product
                         key={product.id}
                         product={product}
                         handlerAddToCart={handlerAddToCart}
